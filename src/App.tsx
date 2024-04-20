5c686468ebdf298ec2e8610059a9d57c2add118e
Gabriel Alejandro Lopez <electromedico@hotmail.com>
2024-04-19 22:44:38 -0400

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Blog from "./components/Blog";
+import MainApp from "./components/MainApp";
 
 function App() {
-  return <Blog />;
+  return <MainApp />;
 }
 
 export default App;
